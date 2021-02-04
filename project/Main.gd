2ee956bd38f93272741371bcdfe1a807d69614a5
suptimal <kaersten.m@web.de>
2021-02-04 10:43:36 +0100

    added Item Menu and mockup for better item menu

--- project/Main.gd
+++ project/Main.gd
@@ -1,14 +1,28 @@
-extends GraphEdit
+extends Control
 
-onready var header_box = get_zoom_hbox()
+#onready var header_box = get_zoom_hbox()
 onready var item_list_container = $CenterContainer
 
 func _ready():
-	var item_list_toggle = Button.new()
-	header_box.add_child(item_list_toggle)
-	item_list_toggle.text = "<<< ITEMLIST >>>"
-	
-	item_list_toggle.connect("pressed", self, "toggle_item_list")
+#	var item_list_toggle = Button.new()
+#	header_box.add_child(item_list_toggle)
+#	item_list_toggle.text = "<<< ITEMLIST >>>"
 #
+#	item_list_toggle.connect("pressed", self, "toggle_item_list")
+
+	pass
 func toggle_item_list():
-	item_list_container.visible = not item_list_container.visible
+	var isvis = not item_list_container.visible
+	item_list_container.visible = isvis
+	var anchor_pos = 1
+	if isvis:
+		anchor_pos = 0
+	item_list_container.anchor_left = anchor_pos
+	item_list_container.anchor_top = anchor_pos
+	
+	
+
+
+func _on_GraphEdit_popup_request(position):
+	toggle_item_list()
+	pass # Replace with function body.
